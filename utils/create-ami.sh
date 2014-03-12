8355db8062359c69e6ecf9ece07031ff20a70552
Stu Teasdale <stuart.teasdale@navetas.com>
2014-03-12 15:25:16 +0000

    refactor to use euca2ools

--- utils/create-ami.sh
+++ utils/create-ami.sh
@@ -13,6 +13,10 @@ function die() {
 
 [ $EUID -eq 0 ] || die "must be root"
 
+export EC2_URL=https://ec2.amazonaws.com/
+
+KERNELID=aki-b4aa75dd 
+
 _basedir="$( cd $( dirname -- $0 )/.. && /bin/pwd )"
 
 cachedir="${_basedir}/cache"
@@ -33,12 +37,12 @@ name="$( basename $config | sed -r -e 's#\.[^.]+$##g' )"
 dest_img="${name}.img"
 
 ## check for required programs
-which aws >/dev/null 2>&1 || die "need aws"
+#which aws >/dev/null 2>&1 || die "need aws"
 which curl >/dev/null 2>&1 || die "need curl"
-which jq >/dev/null 2>&1 || die "need jq"
 which e2fsck >/dev/null 2>&1 || die "need e2fsck"
 which resize2fs >/dev/null 2>&1 || die "need resize2fs"
 rpm -q python-imgcreate >/dev/null 2>&1 || die "need python-imgcreate package"
+rpm -q euca2ools >/dev/null 2>&1 || die "need euca2ools package"
 
 ## the block device must exist
 [ -e "${block_dev}" ] || die "${block_dev} does not exist"
@@ -48,11 +52,11 @@ my_instance_id="$( curl -s http://169.254.169.254/latest/meta-data/instance-id )
 
 ## set up/verify aws credentials and settings
 ## http://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html
-export AWS_DEFAULT_REGION="$( curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed -e 's#.$##g' )"
-[ -n "${AWS_ACCESS_KEY_ID}" ] || die "AWS_ACCESS_KEY_ID not set"
-[ -n "${AWS_SECRET_ACCESS_KEY}" ] || die "AWS_SECRET_ACCESS_KEY not set"
+export EC2_DEFAULT_REGION="$( curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed -e 's#.$##g' )"
+[ -n "${EC2_ACCESS_KEY}" ] || die "EC2_ACCESS_KEY not set"
+[ -n "${EC2_SECRET_KEY}" ] || die "EC2_SECRET_KEY not set"
 
-if [ "$( aws ec2 describe-volumes --volume-ids ${vol_id} | jq -r .Volumes[].Attachments[].InstanceId )" != "${my_instance_id}" ]; then
+if [ "$( euca-describe-volumes ${vol_id} | grep -Eo 'i-[0-9a-f]+')" != "${my_instance_id}" ]; then
     die "volume ${vol_id} is not attached to this instance!"
 fi
 
@@ -77,16 +81,15 @@ e2fsck -f ${block_dev}1
 resize2fs ${block_dev}1
 
 ## create a snapshot of the volume
-snap_id=$( aws ec2 create-snapshot --volume-id ${vol_id} --description "root image for ${name}" | jq -r .SnapshotId )
+snap_id=$( euca-create-snapshot --description "root image for ${name}" ${vol_id} | grep -Eo 'snap-[0-9a-f]+' )
 
-while [ $( aws ec2 describe-snapshots --snapshot-ids ${snap_id} | jq -r .Snapshots[].State ) != "completed" ]; do
+while [ "$( euca-describe-snapshots ${snap_id} | awk '{ print $4 }' )" != "completed" ]; do
     echo "waiting for snapshot ${snap_id} to complete"
     sleep 5
 done
 
 ## kernel-id hard-coded
 ## see http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/UserProvidedKernels.html
-## fuck me, bash space escaping is a pain in the ass.
-image_id=$( aws ec2 register-image --kernel-id aki-919dcaf8 --architecture x86_64 --name "${ami_name}" --root-device-name /dev/sda1 --block-device-mappings "[{\"DeviceName\":\"/dev/sda\",\"Ebs\":{\"SnapshotId\":\"${snap_id}\",\"VolumeSize\":10}},{\"DeviceName\":\"/dev/sdb\",\"VirtualName\":\"ephemeral0\"}]" | jq -r .ImageId )
+image_id=$( euca-register --kernel ${KERNELID} --architecture x86_64 --name "${ami_name}" --root-device-name /dev/sda1 --block-device-mapping="/dev/sda=${snap_id}:6" --block-device-mapping="/dev/sdb=ephemeral0" | grep -Eo 'ami-[0-9a-f]+' )
 
 echo "created AMI with id ${image_id}"
